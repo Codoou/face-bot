37fc75a18893e89a118360df65e769641d74ca72
Cody <cody.w.zimmerman@gmail.com>
2022-03-18 21:26:00 -0500

    Fixed bot where if I mentioned myself, bot woudl continue to repost non-stop

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from tabnanny import check
 from app.andrew_based import AndrewBased
 from app.twitter import Twitter
 import requests
@@ -5,6 +6,26 @@ import json
 import sys
 
 FILESTORAGE = "replied_mentions.txt"
+ME = "codyzdotorg".lower()
+
+def add_id(id):
+    with open(FILESTORAGE, "a") as e:
+        e.write(id+'\n')
+
+def check_if_id_exists(id):
+    # logic for not repeating mentions
+    # Exits if it does to prevent multiple runs per id
+    with open(FILESTORAGE, "r") as f:
+        """"""
+        for x in f:
+            if x.strip() == str(id).strip():
+                print("we done did it")
+                sys.exit(0)
+
+def check_if_self(screen_name):
+    if screen_name.lower() == ME:
+        print("Hey I did the last mention, I will stop now")
+        sys.exit(0)
 
 t = Twitter()
 a = AndrewBased()
@@ -13,25 +34,20 @@ response = t.get_most_recent_mention()
 
 tweet_json = json.loads(json.dumps(response[0]._json))
 
+screen_name = tweet_json['user']['screen_name']
+id = tweet_json["id_str"]
+
 try:
     image_url = tweet_json['entities']['media'][0]['media_url']
-    screen_name = tweet_json['user']['screen_name']
-    id = tweet_json["id_str"]
 except Exception as ex:
     print("Unable to get media. Probably no media")
+    add_id(id)
     raise ex
 
-# logic for not repeating mentions
-with open(FILESTORAGE, "r") as f:
-    """"""
-    #f.read()
-    for x in f:
-        if x == id:
-            print("we done did it")
-            sys.exit(0)
+check_if_id_exists(id)
+add_id(id)
+check_if_self(screen_name=screen_name)
 
-with open(FILESTORAGE, "a") as e:
-    e.write(id)
 
 img_data = requests.get(image_url).content
 
